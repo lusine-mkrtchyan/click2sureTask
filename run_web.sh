4f4deed12d0cb59d5295b14bddef049e59502db4
093lus <lusine.mkrtchyan93@gmail.com>
2019-01-11 03:06:36 -0500

    deployment issue fix

--- run_web.sh
+++ run_web.sh
@@ -9,5 +9,6 @@ python manage.py makemigrations
 # migrate db, so we have the latest db schema
 python manage.py migrate
 # start development server on public ip interface, on port 8000
-python manage.py runserver 0.0.0.0:8000
+####  python manage.py runserver 0.0.0.0:8000
+gunicorn task.wsgi -b 0.0.0.0:8000
 run_celery.sh
--- task/task/settings.py
+++ task/task/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/home/ubuntu/click2sureTask/task/static"
+
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
